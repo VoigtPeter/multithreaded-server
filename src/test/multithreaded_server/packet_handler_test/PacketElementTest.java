a621b58b2d3e022e603918694b0194f5cda8ff97
vope1234 <voigt.peter2001@web.de>
2018-04-10 22:16:08 +0200

    added tests

---
+++ src/test/multithreaded_server/packet_handler_test/PacketElementTest.java
@@ -0,0 +1,51 @@
+package multithreaded_server.packet_handler_test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import multithreaded_server.packet_handler.PacketElement;
+
+/**
+ * PacketElementTest
+ * 
+ * @since 0.2.0
+ * @version 0.2.0
+ * @author Peter Voigt
+ *
+ */
+public class PacketElementTest {
+	
+	@Test
+	public void testPacketData() {
+		byte[] testData = "test data".getBytes();
+		
+		PacketElement packetElement = new PacketElement();
+		packetElement.setData(testData);
+		Assert.assertArrayEquals(testData, packetElement.getData());
+		
+		packetElement = new PacketElement(testData, 0);
+		Assert.assertArrayEquals(testData, packetElement.getData());
+	}
+	
+	@Test
+	public void testPacketType() {
+		int testType = PacketElement.OTHER;
+		
+		PacketElement packetElement = new PacketElement();
+		packetElement.setType(testType);
+		Assert.assertSame(testType, packetElement.getType());
+		
+		packetElement = new PacketElement(null, testType);
+		Assert.assertSame(testType, packetElement.getType());
+	}
+	
+	@Test
+	public void testPacketDescription() {
+		String testDescription = "test description";
+		
+		PacketElement packetElement = new PacketElement();
+		packetElement.setDescription(testDescription);
+		Assert.assertTrue(testDescription.equals(packetElement.getDescription()));
+	}
+	
+}
---
+++ src/test/multithreaded_server/util_test/BinaryEncoderDecoderTest.java
@@ -0,0 +1,43 @@
+package multithreaded_server.util_test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import multithreaded_server.util.BinaryDecoder;
+import multithreaded_server.util.BinaryEncoder;
+
+/**
+ * BinaryEncoderDecoderTest
+ * 
+ * @since 0.2.0
+ * @version 0.2.0
+ * @author Peter Voigt
+ *
+ */
+public class BinaryEncoderDecoderTest {
+
+	@Test
+	public void testEncodeDecodeNumbers() {
+		String numbers = "1234567890";
+		Assert.assertTrue(numbers.equals(BinaryDecoder.decodeString(BinaryEncoder.encodeString(numbers))));
+	}
+	
+	@Test
+	public void testEncodeDecodeLowerCaseLetters() {
+		String lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
+		Assert.assertTrue(lowerCaseLetters.equals(BinaryDecoder.decodeString(BinaryEncoder.encodeString(lowerCaseLetters))));
+	}
+	
+	@Test
+	public void testEncodeDecodeUpperCaseLetters() {
+		String upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		Assert.assertTrue(upperCaseLetters.equals(BinaryDecoder.decodeString(BinaryEncoder.encodeString(upperCaseLetters))));
+	}
+	
+	@Test
+	public void testEncodeDecodeSymbols() {
+		String symbols = ".-:+=^!/*?&<>()[]{}@%$#";
+		Assert.assertTrue(symbols.equals(BinaryDecoder.decodeString(BinaryEncoder.encodeString(symbols))));
+	}
+	
+}
